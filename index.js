3c35a314d4f54e67d43d6a9a57cfd6de95cf0a91
KouKiriyama03 <josuepanky@gmail.com>
2020-10-10 22:38:52 -0600

    Implementación de express-handlebars como tmplate engine y creación del end point

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ const express = require("express");
 
 // Crear un servidor express
 const app = express();
+// Importar handlebars como template engine
+const exphbs = require("express-handlebars");
+// crear un servidor express
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: "hbs" }));
+
+app.set("view engine", "hbs");
+
+// 
+
+
 //Creación de la ruta
 app.use("/", (req, res, next) => {
-    res.send("¡¡¡TAREA!!!");
+    res.render("home");
 });
 
 // Inicializar el servidor en un puerto en específico
     app.listen(5005, () => {
         console.log("Servidor ejecutándose en el puerto 5005");
-    });
+    });   
